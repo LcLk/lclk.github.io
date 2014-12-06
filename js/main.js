b87ea5cfcf687aecf42aedce7420e606b2b32471
Liam Krewer <liam.krewer@gmail.com>
2014-12-06 00:29:58 +0000

    first design, blog, home and profile
    added angular, gsap, css etc. Initialized routing for spa app, basic
    link highlighting. let's hope this design doesn't change too much

---
+++ js/main.js
@@ -0,0 +1,64 @@
+app = angular.module('MyApp',['ngRoute']);
+
+console.log("app loaded")
+
+app.config( function($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('!');
+    $routeProvider.
+      when("/home", { 
+        templateUrl: "home/index.html",
+        controller: "HomeCtrl"
+      }).
+      when("/blog", { 
+        templateUrl: "blog/index.html",
+        controller: "BlogCtrl"
+      }).
+      when("/profile", { 
+        templateUrl: "profile/index.html",
+        controller: "ProfileCtrl"
+      }).
+      otherwise( { 
+        redirectTo: "/home" 
+      });
+});
+
+app.controller('NavCtrl', function($scope,$location){
+  $scope.test = "Hello World";
+  $scope.sections = [
+    {
+      url:"home",
+      text:"Home",
+      icon: "home",
+      color: "blue"
+    },
+    {
+      url:"blog",
+      text:"Blog",
+      icon: "paragraph",
+      color: "yellow"
+    },
+    {
+      url:"profile",
+      text:"Profile",
+      icon: "user",
+      color: "green"
+    }
+  ];
+
+  // Is the current tab the active partial?
+  $scope.isActive = function(path) {
+    if ($location.path().substr(1, path.length) == path) {
+      return "active"
+    } else {
+      return ""
+    }
+  }
+
+});
+
+app.controller('BlogCtrl', function($scope){
+  $scope.test = "Hello Blog World";
+});
+
+
+
